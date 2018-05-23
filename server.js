5af2a4433e5e2c693d57a996335e015e3c46851a
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-05-23 22:49:58 +0530

    Fix MIME error

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render('index');
 });
 app.get('/:date', (req, res) => {
   var unixDate = 0;
   var naturalDate = '';
-  var query = req.params.id;
-  console.log(query); // eslint-disable-line
+  var query = req.params.date;
+  res.send(query);
 });
 app.listen(process.env.PORT || 3000);
